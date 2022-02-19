aee40c965eb80d28cd0374d35036db1a9fc26656
kirillkychkin <kychkin.kirill@gmail.com>
2022-02-20 02:36:28 +0900

    initial commit

---
+++ Helpers.cpp
@@ -0,0 +1,6 @@
+#include "Helpers.h"
+
+int Helpers::SquareSum(int num1, int num2)
+{
+	return (num1 + num2)*(num1 + num2);
+}
---
+++ Helpers.h
@@ -0,0 +1,7 @@
+#pragma once
+class Helpers
+{
+	public:
+		static int SquareSum(int num1, int num2);
+};
+
